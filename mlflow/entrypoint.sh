c6faef88c40876a83303e45f8bedc8622e7a7598
chochtthesecond <114176074+chochtthesecond@users.noreply.github.com>
2025-10-27 19:49:14 +0300

    Update entrypoint.sh

--- mlflow/entrypoint.sh
+++ mlflow/entrypoint.sh
@@ -4,4 +4,5 @@ mlflow server \
   --backend-store-uri=${STORE_URI} \
   --artifacts-destination=${ARTIFACTS_DESTINATION} \
   --host 0.0.0.0 \
-  --port=5000
+  --port=5000 \
+  --config-file /app/mlflow.conf
