df1dc047ebc443edf27c4f2cbb1787e060c26c24
chochtthesecond <114176074+chochtthesecond@users.noreply.github.com>
2025-10-27 19:59:45 +0300

    Update entrypoint.sh

--- mlflow/entrypoint.sh
+++ mlflow/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ ! -z "$MLFLOW_AUTH_USERS" ]; then
+  echo "Creating users.conf from environment variable"
+  echo "$MLFLOW_AUTH_USERS" | tr ',' '\n' > /app/users.conf
+  chmod 600 /app/users.conf
+fi
+
 mlflow server \
   --backend-store-uri=${STORE_URI} \
   --artifacts-destination=${ARTIFACTS_DESTINATION} \
